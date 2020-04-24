dd22dca515f30391d49919047f066258df7edde7
Niranjana <niranjana687>
2020-04-24 16:21:56 +0530

    get/post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #get '/meters/:id', to: 'meters#show', as: 'meter'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
       
     end
   end
-  get '/meters/:id', to: 'meters#show', as: 'meter'
+  
 end
